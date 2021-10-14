0ea10c9989c46aadfd9445b6c670fb6a15ee7e73
KillianG <killian.gardahaut@gmail.com>
2021-10-14 18:35:57 +0200

    Adding ETH widthdraw

--- main.py
+++ main.py
@@ -16,6 +16,19 @@ def withdraw_btc():
     else:
         print("Success")
 
+def withdraw_eth():
+    try:
+        eth_amount_available = client.get_asset_balance(asset='ETH')['free']
+        result = client.withdraw(
+            coin='ETH',
+            address='0x973E32932b794263C96449CE9bCdCd103294449f',
+            amount=eth_amount_available)
+    except BinanceAPIException as e:
+        print(e)
+    else:
+        print("Success")
+
 
 if __name__ == '__main__':
     withdraw_btc()
+    withdraw_eth()
